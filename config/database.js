f2740f0996007217a5b036e76e04acce9cd74d8b
ceejayps <ceejayps1308@gmail.com>
2023-01-24 22:03:03 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,23 +1,115 @@
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'mongoose',
+//       settings: {
+//         uri: "mongodb:localhost/wahnexttest",
+//         srv:  !true,
+//         port: 27017, //env.int('DATABASE_PORT', 1874),
+//         database: 'wahnexttest'//"arcadeKid",
+//       },
+//       options: {
+//         authenticationDatabase: null,
+//         ssl: false,
+//       },
+//     },
+//   },
+// })
+
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'mongoose',
+//       settings: {
+//         host: env('DATABASE_HOST'),
+//         srv: env.bool('DATABASE_SRV', true),
+//         port: env.int('DATABASE_PORT', 27017),
+//         database: env('DATABASE_NAME'),
+//         username: env('DATABASE_USERNAME'),
+//         password: env('DATABASE_PASSWORD'),
+//       },
+//       options: {
+//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+//         ssl: env.bool('DATABASE_SSL', true),
+//       },
+//     },
+//   },
+// });"mongodb+srv://root:password@mongodb:27017/test?retryWrites=true&w=majority"
+
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
+        host: "mongodb",//env('DATABASE_HOST'),
+        srv: !true,
+        port:  27017,
+        database: 'text',
+        username: "root",
+        password: "password",
+      
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        authenticationDatabase: null,
+        ssl:  !true,
       },
     },
   },
-});
+});//"mongodb+srv://root:password@mongodb:27017/test?retryWrites=true&w=majority"
+
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'mongoose',
+//       settings: {
+//         uri: "mongodb+srv://root:password@0.0.0.0:27017/test?retryWrites=true&w=majority",
+//         srv: true,
+//         port: 27017, //env.int('DATABASE_PORT', 1874),
+//         database: 'text'//"arcadeKid",
+//       },
+//       options: {
+//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+//         ssl: env.bool('DATABASE_SSL', false),
+//       },
+//     },
+//   },
+// })
+
+
+
+
+
+
+
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'mongoose',
+//       settings: {
+//         host: env('DATABASE_HOST'),
+//         srv: env.bool('DATABASE_SRV', true),
+//         port: env.int('DATABASE_PORT', 27017),
+//         database: env('DATABASE_NAME'),
+//         username: env('DATABASE_USERNAME'),
+//         password: env('DATABASE_PASSWORD'),
+//       },
+//       options: {
+//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+//         ssl: env.bool('DATABASE_SSL', true),
+//       },
+//     },
+//   },
+// });
 
 
 //local testing
